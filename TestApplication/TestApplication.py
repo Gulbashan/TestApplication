86535c08f4a440348d51ad13a5375700914275be
Gulbashan <gulapsakhan415@gmail.com>
2025-09-07 20:49:13 +0530

    Update TestApplication.py
    many print statement from github

--- TestApplication/TestApplication.py
+++ TestApplication/TestApplication.py
@@ -1,3 +1,7 @@
 
 print("My First app on GitHub")
 print("Hello world from Github")
+print("My First app on GitHub")
+print("Hello world from Github")
+print("My First app on GitHub")
+print("Hello world from Github")
